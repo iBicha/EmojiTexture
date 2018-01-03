30904b68d16847641fb4bda2e706a2e915b8bc71
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-01-03 02:19:37 -0700

    use one static UILabel, shared between all textures

--- Assets/EmojiTexture/Plugins/iOS/EmojiTexture.mm
+++ Assets/EmojiTexture/Plugins/iOS/EmojiTexture.mm
@@ -1,5 +1,21 @@
 #import <Foundation/Foundation.h>
 
+UILabel * getUILabel() {
+    static UILabel * label = nil;
+    if (label == nil)
+    {
+        label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 256, 256)];
+        [label setOpaque:NO];
+        label.font = [UIFont systemFontOfSize:999];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+        label.lineBreakMode = NSLineBreakByClipping;
+        label.minimumScaleFactor = 0.0001;
+        label.adjustsFontSizeToFitWidth = YES;
+    }
+    return label;
+}
+
 extern "C" {
     
     unsigned char * EmojiTexture_alloc(int size)
@@ -23,16 +39,8 @@ extern "C" {
                                                      kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
         CGContextClearRect(context, CGRectMake(0, 0, width, height));
         if(text){
-            UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, width, height)];
-            
-            [label setOpaque:NO];
-            label.font = [UIFont systemFontOfSize:999];
-            label.textAlignment = NSTextAlignmentCenter;
-            label.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
-            label.lineBreakMode = NSLineBreakByClipping;
-            label.minimumScaleFactor = 0.0001;
-            label.adjustsFontSizeToFitWidth = YES;
-
+            UILabel * label = getUILabel();
+            [label setFrame:CGRectMake(0,0,width,height)];
             label.text = [NSString stringWithUTF8String: text];
             [label.layer renderInContext:context];
         }
