11cc42efa04f551adfd05b416ee91e1428ad1b10
Brahim Hadriche <brahimh@3dprintwestern.com>
2018-02-14 10:28:35 -0700

    Sanitize emoji text, added keyboard example

--- Assets/EmojiTexture/EmojiTexture.cs
+++ Assets/EmojiTexture/EmojiTexture.cs
@@ -23,6 +23,23 @@ public class EmojiTexture
 #endif
 
     /// <summary>
+    /// Get or set how the text if filtered. If set to true, the first emoji character found is extracted from the text to be rendered.
+    /// If set to false, text is rendered as is. Defaults to true.
+    /// </summary>
+    public bool SanitizeText
+    {
+        get
+        {
+            return sanitizeText;
+        }
+        set
+        {
+            sanitizeText = value;
+            this.Text = Text;
+        }
+    }
+
+    /// <summary>
     /// Get or set the text of the emoji. Usually a one character string representing the emoji.
     /// </summary>
     /// <value>Emoji string</value>
@@ -34,24 +51,11 @@ public class EmojiTexture
         }
         set
         {
-            if (value != text)
-            {
-                text = value;
-                //Rendering
-#if UNITY_IOS && !UNITY_EDITOR
-                EmojiTexture_render(text, buffer, texture.width, texture.height);
-#elif UNITY_ANDROID && !UNITY_EDITOR
-                EmojiTextureClass.CallStatic("render", text, jByteBuffer, texture.width, texture.height);
-#endif
-                //Copy pixels to texture
-#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
-                isByteBufferDirty = true;
-                texture.LoadRawTextureData(buffer, bufferSize);
-                texture.Apply();
-#endif
-            }
+            text = SanitizeText ? GetEmojiString(value) : value;
+            Render();
         }
     }
+
     /// <summary>
     /// Copies the pixels from the native buffer and returns a byte array in the RGBA32 format.
     /// </summary>
@@ -72,8 +76,26 @@ public class EmojiTexture
         }
     }
 
+    private void Render()
+    {
+        //Rendering
+#if UNITY_IOS && !UNITY_EDITOR
+        EmojiTexture_render(text, buffer, texture.width, texture.height);
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        EmojiTextureClass.CallStatic("render", text, jByteBuffer, texture.width, texture.height);
+#endif
+        //Copy pixels to texture
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
+        isByteBufferDirty = true;
+        texture.LoadRawTextureData(buffer, bufferSize);
+        texture.Apply();
+#endif
+
+    }
+
     private Texture2D texture;
     private string text;
+    private bool sanitizeText;
     private IntPtr buffer;
     private AndroidJavaObject jByteBuffer;
     private int bufferSize;
@@ -101,6 +123,7 @@ public class EmojiTexture
         buffer = IntPtr.Zero;
         byteBuffer = null;
         isByteBufferDirty = false;
+        sanitizeText = true;
 
 #if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
         bufferSize = width * height * 4;
@@ -126,4 +149,21 @@ public class EmojiTexture
         return emojiTexture == null ? null : emojiTexture.texture;
     }
 
+    /// <summary>
+    /// Extracts the first emoji character from the text. returns String.Empty if no emojis are found.
+    /// </summary>
+    /// <param name="text">The input text</param>
+    /// <returns>A string representing an emoji. String.Empty if not emojis found in the input text.</returns>
+    public static string GetEmojiString(string text)
+    {
+        if (!string.IsNullOrEmpty(text))
+        {
+            var match = System.Text.RegularExpressions.Regex.Match(text, @"(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])");
+            if (match.Success)
+            {
+                return match.Value;
+            }
+        }
+        return String.Empty;
+    }
 }
--- Assets/EmojiTexture/Example/EmojiTextureExample.cs
+++ Assets/EmojiTexture/Example/EmojiTextureExample.cs
@@ -3,14 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class EmojiTextureExample : MonoBehaviour
+public class EmojiTextureExample : MonoBehaviour, IPointerClickHandler
 {
-
+    //Material of the changing emoji texture
     public Material material;
 
+    //EmojiTexture used for rendering
     private EmojiTexture emojiTexture;
 
+    //Emoji list to shuffle from
     private string[] emojis = { "☺", "❤", "🍆", "🍑" };
 
     void Start()
@@ -20,9 +23,45 @@ public class EmojiTextureExample : MonoBehaviour
         InvokeRepeating("ChangeEmoji", 0f, 1f);
     }
 
+    //Change emoji every second
     void ChangeEmoji()
     {
         emojiTexture.Text = emojis[Mathf.RoundToInt(Time.time) % emojis.Length];
+    }
+
+    //When the quad is tapped, we open the touch keyboard to input own emoji
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        StartEditText();
+    }
+
+    public void StartEditText()
+    {
+        StopAllCoroutines();
+        StartCoroutine(EditText());
+    }
+
+    IEnumerator EditText()
+    {
+        if (!TouchScreenKeyboard.isSupported)
+        {
+            yield break;
+        }
+
+        TouchScreenKeyboard keyboard = TouchScreenKeyboard.Open(emojiTexture.Text);
+
+        yield return new WaitUntil(() => {
+            return keyboard.status == TouchScreenKeyboard.Status.Canceled ||
+            keyboard.status == TouchScreenKeyboard.Status.LostFocus ||
+            keyboard.status == TouchScreenKeyboard.Status.Done;
+        });
+        if (keyboard.status == TouchScreenKeyboard.Status.Done)
+        {
+            //Stop shuffling emojis, and display the one entered with the keyboard
+            CancelInvoke("ChangeEmoji");
+            emojiTexture.Text = keyboard.text;
+        }
+
     }
 
 }
